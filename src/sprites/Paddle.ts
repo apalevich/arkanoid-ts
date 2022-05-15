1aff0c4a63eb19034b2a204d326fe531e38c3c12
Artyom Palevich <hello@apalevich.ru>
2022-05-15 18:02:08 +0400

    Filled constructor and setters

--- src/sprites/Paddle.ts
+++ src/sprites/Paddle.ts
@@ -1,5 +1,30 @@
 import { Vector } from "~/types";
 
 export class Paddle {
+    private paddleImage: HTMLImageElement = new Image();
+    private moveLeft: boolean = false;
+    private moveRight: boolean = false;
 
+    constructor(
+        private speed: number,
+        private paddleWidth: number,
+        private paddleHeight: number,
+        private position: Vector,
+        image: string
+    ) {
+        this.speed = speed;
+        this.paddleWidth = paddleWidth;
+        this.paddleHeight = paddleHeight;
+        this.position = position;
+        this.moveLeft = false;
+        this.moveRight = false;
+        this.paddleImage.src = image;
+    }
+
+    get width(): number { return this.paddleWidth };
+    get height(): number { return this.paddleHeight };
+    get pos(): Vector { return this.position };
+    get image(): HTMLImageElement { return this.image };
+    get isMovingLeft(): boolean { return this.moveLeft };
+    get isMovingRight(): boolean { return this.moveRight };
 }
